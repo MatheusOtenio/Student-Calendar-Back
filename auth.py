54f0552d3b9aa7b1656ec8120f197d822bbfde11
Matheus Otenio <matheus.otenio843@gmail.com>
2025-04-18 17:07:20 -0300

    att auth debug

--- auth.py
+++ auth.py
@@ -12,8 +12,12 @@ from database import get_db
 import models
 import crud
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Configurações de segurança
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"  # Deve ser substituído por uma chave segura em produção
+SECRET_KEY = os.getenv("SECRET_KEY", "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7")  # Deve ser substituído por uma chave segura em produção
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
